2beb3deed5c0cad6147d3b8a27a778645b9a306a
WaiPhyiAung <ww.phyo29aung@gmail.com>
2023-10-03 15:24:01 +0630

    added remote repo

--- src/main/MainSection.java
+++ src/main/MainSection.java
@@ -5,7 +5,7 @@ public class MainSection {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Inside git_practice2/main/MainSection.java");
-
+		System.out.println("pushed on remote repository");
 	}
 
 }
